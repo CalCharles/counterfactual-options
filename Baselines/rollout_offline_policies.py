5d0c45b2b61d7663340a97fa42056d2667279fda
aditya-arjun <aditya.arjun123@gmail.com>
2022-02-12 16:12:19 -0600

    add rollout script

--- Baselines/rollout_offline_policies.py
+++ Baselines/rollout_offline_policies.py
@@ -24,18 +24,10 @@ from Baselines.env_args import add_env_args
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--algorithm', choices=['dqn', 'rainbow', 'sac'])
-
-
-    parser.add_argument('--seed', type=int, default=238)
-    parser.add_argument('--eps-test', type=float, default=0.005)
-    parser.add_argument('--eps-train', type=float, default=1.)
-    parser.add_argument('--eps-train-final', type=float, default=0.05)
-    parser.add_argument('--buffer-size', type=int, default=100000)
-    parser.add_argument('--video-log-period', type=int, default=5)
-    parser.add_argument('--save-checkpoint-period', type=int, default=None)
-
+    parser.add_argument('--model-path', type=str)
 
     # DQN/Rainbow Arguments
+    parser.add_argument('--eps-test', type=float, default=0.005)
     parser.add_argument('--lr', type=float, default=0.0001)
     parser.add_argument('--num-atoms', type=int, default=51)
     parser.add_argument('--v-min', type=float, default=-10.)
@@ -65,30 +57,11 @@ def get_args():
     parser.add_argument('--n-step', type=int, default=1)
 
 
-    # Shared Arguments
-    parser.add_argument('--epoch', type=int, default=100)
-    parser.add_argument('--step-per-epoch', type=int, default=100000)
-    parser.add_argument('--step-per-collect', type=int, default=10)
-    parser.add_argument('--update-per-step', type=float, default=0.1)
-
-
-    parser.add_argument('--batch-size', type=int, default=32)
-    parser.add_argument('--test-num', type=int, default=10)
-    parser.add_argument('--logdir', type=str, default='log')
-    parser.add_argument(
-        '--device', type=str, default='cuda' if torch.cuda.is_available() else 'cpu'
-    )
-    parser.add_argument('--resume-path', type=str, default=None)
-    parser.add_argument('--resume-id', type=str, default=None)
-    parser.add_argument(
-        '--watch',
-        default=False,
-        action='store_true',
-        help='watch the play of pre-trained policy only'
-    )
-    parser.add_argument('--save-buffer-name', type=str, default=None)
-
     add_env_args(parser)
+
+    # Rollout Args
+    parser.add('--num-episodes', type=int, default=10)
+    parser.add('--output-path', type=str, default='rollout.mp4')
 
     return parser.parse_args()
 
@@ -101,7 +74,6 @@ def test(args=get_args()):
     print("Observations shape:", args.state_shape)
     print("Actions shape:", args.action_shape)
     # make environments
-    train_envs = DummyVectorEnv([make_breakout_env_fn(args)])
     test_envs = DummyVectorEnv([make_breakout_env_fn(args)])
     # seed
     np.random.seed(args.seed)
@@ -221,142 +193,30 @@ def test(args=get_args()):
     else:
         raise("Must specify valid algorithm to offline baseline trainer")
 
+    policy.load_state_dict(torch.load(args.model_path, map_location=args.device))
+    print("Loaded agent from: ", args.model_path)
 
-    # load a previous policy
-    if args.resume_path:
-        policy.load_state_dict(torch.load(args.resume_path, map_location=args.device))
-        print("Loaded agent from: ", args.resume_path)
+    policy.eval()
+    if args.algorithm in ['rainbow', 'dqn']:
+        policy.set_eps(args.eps_test)
 
-    # collector
-    train_collector = VideoCollector(policy, train_envs, buffer, exploration_noise=args.algorithm in ['rainbow', 'dqn'])
     test_collector = VideoCollector(policy, test_envs, exploration_noise=args.algorithm in ['rainbow', 'dqn'])
-    # log
-    writer = SummaryWriter(log_path)
-    writer.add_text("args", str(args))
-    logger = TensorboardLogger(writer)
 
-    def save_fn(policy):
-        torch.save(policy.state_dict(), os.path.join(log_path, 'policy.pth'))
-
-    def stop_fn(mean_rewards):
-        return False
-
-    def train_fn(epoch, env_step):
-        if args.algorithm in ['rainbow', 'dqn']:
-            # nature DQN setting, linear decay in the first 1M steps
-            if env_step <= 1e6:
-                eps = args.eps_train - env_step / 1e6 * \
-                    (args.eps_train - args.eps_train_final)
-            else:
-                eps = args.eps_train_final
-            policy.set_eps(eps)
-            if env_step % 1000 == 0:
-                logger.write("train/env_step", env_step, {"train/eps": eps})
-
-    def test_fn(epoch, env_step):
-        if args.algorithm in ['rainbow', 'dqn']:
-            policy.set_eps(args.eps_test)
-
-    # watch agent's performance
-    def watch():
-        print("Setup test envs ...")
-        policy.eval()
-
-        if args.algorithm in ['rainbow', 'dqn']:
-            policy.set_eps(args.eps_test)
-
-        test_envs.seed(args.seed)
-        if args.save_buffer_name:
-            print(f"Generate buffer with size {args.buffer_size}")
-            if args.algorithm == 'rainbow':
-                buffer = PrioritizedVectorReplayBuffer(
-                    args.buffer_size,
-                    buffer_num=len(test_envs),
-                    ignore_obs_next=True,
-                    alpha=args.alpha,
-                    beta=args.beta
-                )
-            elif args.algorithm == 'dqn':
-                buffer = VectorReplayBuffer(
-                    args.buffer_size,
-                    buffer_num=len(test_envs),
-                    ignore_obs_next=True,
-                )
-            elif args.algorithm == 'sac':
-                buffer = VectorReplayBuffer(
-                    args.buffer_size,
-                    buffer_num=len(test_envs),
-                )
-
-            collector = VideoCollector(policy, test_envs, buffer, exploration_noise=args.algorithm in ['rainbow', 'dqn'])
-            result = collector.collect(n_step=args.buffer_size, render=0.05)
-            print(f"Save buffer into {args.save_buffer_name}")
-            # Unfortunately, pickle will cause oom with 1M buffer size
-            buffer.save_hdf5(args.save_buffer_name)
-        else:
-            print("Testing agent ...")
-            test_collector.reset()
-            result = test_collector.collect(
-                n_episode=args.test_num, render=0.05
-            )
-
-        rew = result["rews"].mean()
-        print(f'Mean reward (over {result["n/ep"]} episodes): {rew}')
-
-        return result["saved_images"]
-
-
-    def save_checkpoint_fn(epoch, env_step, gradient_step):
-        # see also: https://pytorch.org/tutorials/beginner/saving_loading_models.html
-        ckpt_path = os.path.join(log_path, 'checkpoint.pth')
-        torch.save({'model': policy.state_dict()}, ckpt_path)
-
-        if args.save_checkpoint_period != None and epoch % args.save_checkpoint_period == 0:
-            ckpt_path = os.path.join(log_path, f'checkpoint-epoch{epoch}.pth')
-            torch.save({'model' : policy.state_dict()}, ckpt_path)
-
-        if epoch % args.video_log_period == 0:
-            images = watch()
-            fname = os.path.join(log_path, f'iter-{epoch}.mp4')
-            writer = imageio.get_writer(fname, fps=20)
-
-            for img in images:
-                writer.append_data(img)
-
-            writer.close()
-
-        return ckpt_path
-
-    if args.watch:
-        watch()
-        exit(0)
-
-    # test train_collector and start filling replay buffer
-    train_collector.collect(n_step=args.batch_size)
-    # trainer
-    result = offpolicy_trainer(
-        policy,
-        train_collector,
-        test_collector,
-        args.epoch,
-        args.step_per_epoch,
-        args.step_per_collect,
-        args.test_num,
-        args.batch_size,
-        train_fn=train_fn,
-        test_fn=test_fn,
-        stop_fn=stop_fn,
-        save_fn=save_fn,
-        logger=logger,
-        update_per_step=args.update_per_step,
-        test_in_train=False,
-        resume_from_log=args.resume_id is not None,
-        save_checkpoint_fn=save_checkpoint_fn,
+    test_envs.seed(args.seed)
+    print("Testing agent ...")
+    test_collector.reset()
+    result = test_collector.collect(
+        n_episode=args.test_num, render=0.05
     )
 
-    pprint.pprint(result)
-    watch()
+    writer = imageio.get_writer(args.output_path)
+    for img in result['saved_images']:
+        writer.append_data(img)
 
+    writer.close()
+
+    rew = result["rews"].mean()
+    print(f'Mean reward (over {result["n/ep"]} episodes): {rew}')
 
 if __name__ == '__main__':
     test_dqn(get_args())
